31b8fbda9fc83324880c766e91b26551661258c4
ustsepavy <vladstepovoyvios@gmail.com>
2020-09-29 21:10:23 +0300

    feat: added function realization

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array) {
+    if (!!array && array.length !== 0) {
+        return array.reduce((prev, cur) => {
+            return prev < cur ? prev : cur;
+        });
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!!array && array.length !== 0) {
+        return array.reduce((prev, cur) => {
+            return prev > cur ? prev : cur;
+        });
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!!array && array.length !== 0) {
+        return (
+            array.reduce((prev, cur) => {
+                return (prev += cur);
+            }, 0) / array.length
+        );
+    }
+    return 0;
+};
